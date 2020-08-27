8be0c1fd14df5d1c1f9ca950a64ed312e2527417
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-08-27 00:31:07 -0400

    hrmr

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Switch, Route } from 'react-router';
  
 export default (
-    <Route>
+    // Switch is added in v4 react-router
+    <Switch>
         <Route path='/' />
         <Route path='/services' />
         <Route path='/contact' />
         <Route path='/blog' />
-    </Route>
+        <Route /> // No-match case
+    </Switch>
 );
