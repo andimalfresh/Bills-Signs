38d278c8d73dd7f304083f2b24412f8a641cd647
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-08-27 17:42:18 -0400

    YO META

--- src/sitemap-routes.js
+++ src/sitemap-routes.js
@@ -3,9 +3,9 @@ import { Route } from 'react-router';
  
 export default (
     <Route>
-	<Route path='/' />
-	<Route path='/servies' />
-    <Route path='/contact' />
-    <Route path='/blog' />
+        <Route path='/' />
+        <Route path='/servies' />
+        <Route path='/contact' />
+        <Route path='/blog' />
     </Route>
 );
